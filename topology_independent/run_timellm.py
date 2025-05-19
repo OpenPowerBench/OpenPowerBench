47e6de40aac877fc6432267445f8f8464f835af7
OpenPowerBench <openpowerbench@gmail.com>
2025-05-19 18:30:03 -0500

    Update run_timellm.py

--- topology_independent/run_timellm.py
+++ topology_independent/run_timellm.py
@@ -196,9 +196,9 @@ if __name__ == '__main__':
 
 
     # model define
-    parser.add_argument('--enc_in', type=int, help='encoder input size')
-    parser.add_argument('--dec_in', type=int, help='decoder input size')
-    parser.add_argument('--c_out', type=int, help='output size')
+    parser.add_argument('--enc_in', default=1, type=int, help='encoder input size')
+    parser.add_argument('--dec_in', default=1, type=int, help='decoder input size')
+    parser.add_argument('--c_out', default=1, type=int, help='output size')
     parser.add_argument('--d_model', type=int, help='dimension of model')
     parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
     parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
