fcea101afb646eec6bede980297a5f41439a457f
OpenPowerBench <openpowerbench@gmail.com>
2025-05-23 17:46:56 -0500

    Update paths.py

--- topology_dependent/transient_state/config/paths.py
+++ topology_dependent/transient_state/config/paths.py
@@ -1,9 +1,9 @@
-case_T = r'\IEEE118bus.pwb'
-base_path = r"\IEEE 118"
-out_path = r'\output'
-ctg_filepath = r'\ctg.aux'
+case_T = '\data\IEEE118bus.pwb'
+base_path = '\data\IEEE 118'
+out_path = '\data\output'
+ctg_filepath = '\data\ctg.aux'
 
-gen_p_path_train = r'\gen_p_118.csv'
-gen_q_path_train = r'\gen_q_118.csv'
-ld_p_path_train = r'\load_p_118.csv'
-ld_q_path_train = r'\load_q_118.csv'
+gen_p_path_train = '\gen_p_14.csv'
+gen_q_path_train = '\gen_q_14.csv'
+ld_p_path_train = '\load_p_14.csv'
+ld_q_path_train = '\load_q_14.csv'
